2a1cf32a81b6d40a794c6a673db7f029e789ccb1
Innis <1575270674@qq.com>
2022-11-28 03:15:46 +0800

    feat add run Animation

--- scrpt.js
+++ scrpt.js
@@ -1,6 +1,7 @@
 var codeInput = document.querySelector('#code-input');
 var codeExplanation = document.querySelector('#code-result');
 var okBtn = document.querySelector('#ok-btn');
+var nyanCat = document.querySelector('#nyan-Cat');
 
 codeInput.addEventListener('input', (e) => {
     codeInput.style.height = '100px';
@@ -21,5 +22,53 @@ codeInput.innerHTML = "if (sad() === true) {\n    sad().stop(); \n    beAwesome(
 codeExplanation.innerHTML = "The code checks if the person is sad. If the person is sad, the code makes them stop being sad and become awesome instead.";
 
 okBtn.addEventListener('click', (e) => {
-    console.log("hi");
+    runAnimation();
 });
+
+async function runAnimation() {
+    await runToRight();
+    await rotateY();
+    await runToLeft();
+    await rotateY();
+    await removeClass();
+}
+
+function runToRight() {
+    return new Promise((resolve, reject) => {
+        nyanCat.classList.add('horizTranslateToRight');
+        setTimeout(() => {
+            resolve();
+        }, 2000);
+    });
+}
+
+function rotateY() {
+    return new Promise((resolve, reject) => {
+        nyanCat.classList.add('rotateY');
+        setTimeout(() => {
+            resolve();
+        }, 500);
+    });
+}
+
+function runToLeft() {
+    return new Promise((resolve, reject) => {
+        nyanCat.classList.add('horizTranslateToLeft');
+        setTimeout(() => {
+            resolve();
+        }, 2000);
+    });
+}
+
+function removeClass() {
+    return new Promise((resolve, reject) => {
+        nyanCat.classList.remove('horizTranslateToRight');
+        nyanCat.classList.remove('rotateY');
+        nyanCat.classList.remove('horizTranslateToLeft');
+        nyanCat.classList.add('rotateYY');
+        setTimeout(() => {
+            nyanCat.classList.remove('rotateYY');
+            resolve();
+        }, 500);
+    });
+}
