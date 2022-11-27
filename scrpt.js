9bc3df1ac4e745e1b2196fb85d7fb67b94f4efe8
Innis <1575270674@qq.com>
2022-11-28 04:26:47 +0800

    feat: add fetch

--- scrpt.js
+++ scrpt.js
@@ -1,3 +1,4 @@
+var container = document.querySelector('#container');
 var codeInput = document.querySelector('#code-input');
 var codeExplanation = document.querySelector('#code-result');
 var okBtn = document.querySelector('#ok-btn');
@@ -7,10 +8,12 @@ var nyanCat = document.querySelector('#nyan-Cat');
 codeInput.addEventListener('input', (e) => {
     codeInput.style.height = '100px';
     codeInput.style.height = e.target.scrollHeight + 'px';
+    scrollToBottom();
 });
 codeExplanation.addEventListener('input', (e) => {
     codeExplanation.style.height = '100px';
     codeExplanation.style.height = e.target.scrollHeight + 'px';
+    scrollToBottom();
 });
 
 // click and select all
@@ -24,6 +27,7 @@ codeExplanation.innerHTML = "The code checks if the person is sad. If the person
 
 okBtn.addEventListener('click', (e) => {
     runAnimation();
+    postData();
 });
 
 // disenble cpBtn
@@ -89,4 +93,66 @@ function removeClass() {
             resolve();
         }, 300);
     });
+}
+
+
+
+
+
+
+
+
+async function postData() {
+    let update = {
+        "sentences": codeInput.value,
+    };
+
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(update),
+    };
+
+    console.log("data", update);
+    fetch('http://101.43.138.40:83/response', options)
+        .then(data => {
+            if (!data.ok) {
+                throw Error(data.status);
+            }
+            return data.json();
+        }).then(update => {
+            //console.log(update);
+            codeExplanation.innerHTML = update.response;
+            switchCopyStatus(true);
+            scrollToBottom();
+        }).catch(e => {
+            console.log(e);
+        });
+}
+
+
+function switchCopyStatus(avtive) {
+    if (avtive === true) {
+        cpBtn.classList.add('cp-btn-active');
+    } else {
+        cpBtn.classList.remove('cp-btn-active');
+    }
+}
+
+function heightToTop(ele) {
+    let root = document.body;
+    let height = 0;
+    do {
+        height += ele.offsetTop;
+        ele = ele.offsetParent;
+    } while (ele !== root)
+    return height;
+}
+
+
+function scrollToBottom() {
+    let bottom = document.getElementById("cp-btn");
+    window.scrollTo(0, heightToTop(bottom));
 }
