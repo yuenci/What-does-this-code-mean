bd18d1415675c6e194add6181159e2b03e16af4a
Innis <33682384+yuenci@users.noreply.github.com>
2022-11-30 04:30:21 +0800

    Fix: mix content error

--- scrpt.js
+++ scrpt.js
@@ -151,7 +151,7 @@ async function postData() {
     };
 
     //console.log("data", update);
-    fetch('http://101.43.138.40:83/response', options)
+    fetch('https://enjoycoding.me/api/response', options)
         .then(data => {
             if (!data.ok) {
                 throw Error(data.status);
